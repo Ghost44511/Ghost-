fdb7fb763ad6cea89a64001038318a0ba92a51f2
Ghost44511 <idrissoukarim102@gmail.com>
2026-03-17 16:39:01 +0100

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,7 @@
+import connectToWhatsapp from './prince k/soul.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+(async() => {
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
