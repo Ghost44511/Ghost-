5247dc1462bf2d26bd85d20c50127a1662d633f4
Ghost44511 <idrissoukarim102@gmail.com>
2026-03-18 00:49:11 +0100

    Delete Index.js

--- Index.js
+++
@@ -1,7 +0,0 @@
-import connectToWhatsapp from './prince k/soul.js'
-import handleIncomingMessage from './events/messageHandler.js'
-
-(async() => {
-    await connectToWhatsapp(handleIncomingMessage)
-        console.log('established !')
-})()
