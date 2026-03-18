65c2fadd9c5375c6383840dad6e9d4f8164a14db
Ghost44511 <idrissoukarim102@gmail.com>
2026-03-18 12:00:40 +0100

    Initialize config.js with global settings

---
+++ config.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+global.owner = ['237650554606'];
+global.ownername = 'Prince K';
+global.botname = 'PRINCE-K-MASTER';
+global.prefix = ['.'];
+global.public = false; // Mode privé pour ton utilité seule
+global.sessionName = 'session';
+
+module.exports = global;
