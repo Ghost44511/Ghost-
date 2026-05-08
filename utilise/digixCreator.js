b0511b5560dacc64c4b9201b4ef75f3c793cfa60
Ghost44511 <idrissoukarim102@gmail.com>
2026-05-08 03:09:39 +0100

    Add digixCreator.js for version info response

---
+++ utilise/digixCreator.js
@@ -0,0 +1,9 @@
+const digixV = async (client, msg) => {
+    msg.reply(`
+🚀 *VERSION DIGIX - PRINCE K*
+• Version : 3.0.1
+• Branche : Main/CrewX
+• Status : Stable 🇨🇲
+• Dev : Prince K`);
+};
+module.exports = digixV;
