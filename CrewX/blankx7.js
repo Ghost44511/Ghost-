28fa6aef9e0270f019e7e94e2901b6ff51287f74
Ghost44511 <idrissoukarim102@gmail.com>
2026-05-10 02:13:53 +0100

    Delete CrewX directory

--- CrewX/blankx7.js
+++
@@ -1,6 +0,0 @@
-const blankx7 = async (client, msg) => {
-    // Caractère invisible spécial
-    const invisible = "‎".repeat(500);
-    await client.sendMessage(msg.from, { text: `🤴 *Nettoyage Prince K*\n${invisible}\n_Chat dégagé._` });
-};
-module.exports = blankx7;
